2a0fe70320708df214b9d4dc50c1e62f903c13d5
msarlo <mateussarlo1@outlook.com>
2025-05-15 14:14:30 -0300

    fix: path para o header

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Header } from '../../components/Header';
+import { Header } from '@/components/Header';
 import { LinkButton } from '@/components/LinkButton';
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
