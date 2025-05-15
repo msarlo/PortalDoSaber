7b44efe1e44ce9cce39f39da4f905ad59eb29992
msarlo <mateussarlo1@outlook.com>
2025-05-15 14:22:11 -0300

    fix: path header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Header } from '../components/Header';
+import { Header } from '@/components/Header';
 import { LinkButton } from '@/components/LinkButton';
 
 export default function HomePage() {
