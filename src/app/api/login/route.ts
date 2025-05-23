a8a0b9354e5b6d45a6f4959fd9112d09cb60267b
msarlo <mateussarlo1@outlook.com>
2025-05-23 17:38:38 -0300

    feat: criar API's básicas (acoes, cursos, login e users)

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,15 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { criarUsuario } from '@/app/services/userService';
+import { login } from '@/app/services/userService';
 import { ZodError } from 'zod';
 
-// API de criação de usuário
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const user = await criarUsuario(body);
-    return NextResponse.json(user, { status: 201 });
-  } catch (error) {
+    const user = await login(body);
     
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(
         { 
@@ -23,10 +22,9 @@ export async function POST(req: NextRequest) {
       );
     }
     
-    // Outros erros
     return NextResponse.json(
-      { error: error instanceof Error ? error.message : 'Erro interno' },
-      { status: 500 }
+      { error: error instanceof Error ? error.message : 'Credenciais inválidas' },
+      { status: 401 }
     );
   }
 }
