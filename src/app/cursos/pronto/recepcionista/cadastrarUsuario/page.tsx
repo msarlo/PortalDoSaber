74c5602e50a33972e24d061bfde0d04158df0408
theml <teddelol247@gmail.com>
2025-06-03 18:26:06 -0300

    fix: Ajustando tutorial para tela e adicionando botão de like e dislike.

--- src/app/cursos/pronto/recepcionista/cadastrarUsuario/page.tsx
+++ src/app/cursos/pronto/recepcionista/cadastrarUsuario/page.tsx
@@ -1,15 +1,18 @@
+"use client";
 import React from "react";
-import Passo1 from "/assets/img/CadastrUsuario21.png"
-import Passo2 from "/assets/img/CadastrUsuario22.png"
-import Passo3 from "/assets/img/CadastrUsuario23.png"
+import Image from "next/image";
+import Passo1 from "/public/assets/img/CadastrUsuario21.png";
+import Passo2 from "/public/assets/img/CadastrUsuario22.png";
+import Passo3 from "/public/assets/img/CadastrUsuario23.png";
+import InteractionButtons from "@/components/InteractionButtons";
 
 export default function CadastrarUsuarioPage() {
-    return (
-        <main style={{ padding: "2rem" }}>
-            <h1>Tutorial aqui</h1>
-            <img src={Passo1} alt="Primeiro Passo" />
-            <img src={Passo2} alt="Segundo Passo" />
-            <img src={Passo3} alt="Terceiro Passo" />
-        </main>
-    );
+  return (
+    <main style={{ padding: "2rem" }}>
+      <Image src={Passo1} alt="Primeiro Passo" />
+      <Image src={Passo2} alt="Segundo Passo" />
+      <Image src={Passo3} alt="Terceiro Passo" />
+      <InteractionButtons initialLikes={0} initialDislikes={0} />
+    </main>
+  );
 }
