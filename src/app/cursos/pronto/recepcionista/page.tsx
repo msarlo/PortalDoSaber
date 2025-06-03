0ec034123dcaf16fc5115a8e14ea54d49dde2446
theml <teddelol247@gmail.com>
2025-06-03 17:10:01 -0300

    Fix: corrigindo erro de rebase/atualização da branch

--- src/app/cursos/pronto/recepcionista/page.tsx
+++ src/app/cursos/pronto/recepcionista/page.tsx
@@ -1,12 +1,37 @@
 import { Banner } from "@/components/Banner";
-import logoProntoBanner from "/public/assets/images/LogoProntoSemBG.png"; // Importe a imagem
+import { Container } from '@/components/Container';
+import { tutoriaisProntoRecepicionista,Tutorial } from '@/lib/prontoData';
+import logoProntoBanner from '/public/assets/images/LogoProntoSemBG.png';
 
 export default function HomePage() {
   return (
-    <main style={{ padding: "2rem" }}>
+    <main className="min-h-screen bg-gray-100">
       <Banner
       title={logoProntoBanner}
         descricao="Tutoriais do Pronto para Recepcionistas"/>
+
+       {/* Barra de Pequisa COMPONENTIZAR DEPOIS*/}
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="mb-8 flex justify-center">
+          <input
+            type="text"
+            placeholder="Pesquisar..."
+            className="p-3 border border-gray-300 rounded-md shadow-sm w-full max-w-lg focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        {/* Grid de Profissões */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
+          {tutoriaisProntoRecepicionista.map((tutorial: Tutorial) => (
+            <Container
+              key={tutorial.id}
+              img={tutorial.imagemSrc}
+              buttonText={tutorial.titulo}
+              href={`/cursos/pronto/${tutorial.profissaoSlug}/${tutorial.slug}`}
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
