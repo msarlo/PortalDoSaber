6e157dc59f5694b797dd6e15656d4d62d3c32d78
theml <teddelol247@gmail.com>
2025-06-03 15:39:24 -0300

    feat: Adicionado tela para recepcionista e adicionando Proffisionais a tela do Pronto.

--- src/app/cursos/pronto/recepcionista/page.tsx
+++ src/app/cursos/pronto/recepcionista/page.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Banner } from '@/components/Banner'; 
-import { Container } from '@/components/Container';
-import { tutoriaisProntoRecepicionista,Tutorial } from '@/lib/prontoData';
-import logoProntoBanner from '/public/assets/images/LogoProntoSemBG.png'; // Importe a imagem
+import React from "react";
+import { Banner } from "@/components/Banner";
+import { Container } from "@/components/Container";
+import { tutoriaisProntoRecepicionista, Tutorial } from "@/lib/prontoData";
+import logoProntoBanner from "/public/assets/images/LogoProntoSemBG.png"; // Importe a imagem
 
 export default function CursosProntoPage() {
   return (
     <main className="min-h-screen bg-gray-100">
-      <Banner 
-        title={logoProntoBanner} 
+      <Banner
+        title={logoProntoBanner}
         descricao="Nossa plataforma foi criada para ajudar profissionais a navegar por processos e ações de forma simples e eficiente. Explore a partir do seu cargo e encontre o que você precisa!"
       />
 
---
+++ src/app/cursos/pronto/recepcionista/tutoriais/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <section className="max-w-7xl mx-auto px-4 py-8">
+        <h1 className="text-2xl font-bold text-gray-800">Página de Tutoriais para o Recepcionista</h1>
+        <p className="mt-4 text-gray-600">Conteúdo específico para recepcionistas será adicionado em breve.</p>
+      </section>
+    </main>
+  );
+}
